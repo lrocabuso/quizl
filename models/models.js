dca55f407d8243904f046527dee5ce0b2c443da8
lrocabuso <lrocabuso@gmail.com>
2015-06-19 20:16:30 +0200

    despliege DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,12 +1,37 @@
 // Necesario para crear las rutas
 var path = require('path');
 
+// Añadimos una serie de pasos iniciales para obtener cada uno de los datos necesarios para
+// realizar la conexión a la BD a partir de la variable de entorno DATABASE_URL
+// Si estamos en producción en modo local se cargará el patrón de SQLite y si estamos en Heroku
+// se cargará el patrón de Postgres
+// Postgres: DATABASE_URL = postgres://user:password@host:port/database
+// SQLite: DATABASE_URL = sqlite://:@://
+
+// Desglosamos el contenido de la variable de entorno con el patrón
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6] || null);
+var user     = (url[2] || null);
+var pwd      = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect  = (url[1] || null);
+var port     = (url[5] || null);
+var host     = (url[4] || null);
+var storage  = process.env.DATABASE_STORAGE;
+
+
 // Cargar el modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar la BD SQLite por medio del modulo Sequelize
-var sequelize = new Sequelize(null, null, null,
-    {dialect: 'sqlite', storage: 'quiz.sqlite'}
+var sequelize = new Sequelize(DB_name, user, pwd,
+    {dialect: protocol,
+      protocol: protocol,
+      port: port,
+      host: host,
+      storage: storage, // Solo SQLite (.env)
+      omitNULL: true    // Solo Postgres
+    }
 );
 
 // Importar la definición de la tabla Quiz definida en el fichero quiz.js
